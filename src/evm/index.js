57cd9a7e78785cf1c902b54faf4694f806e43ca1
Chris Hitchcott <hitchcott@gmail.com>
2018-11-24 20:42:03 +0700

    add contract API

--- src/evm/index.js
+++ src/evm/index.js
@@ -1,4 +1,6 @@
-import web3 from './web3';
+import web3, { contract } from './web3';
+
+const gasPrice = 1;
 
 export async function getAccounts() {
   const accounts = await web3.eth.getAccounts();
@@ -20,4 +22,14 @@ export async function signMessage(message) {
 export function messageIsValid(address, data, signature) {
   const signer = web3.eth.personal.ecRecover(data, signature);
   return signer === address;
-} 
+}
+
+// createSwap('0x0f18cd0F5B7CcE9d6DCC246F80B0fCdd7a2AF150', 1, '0x5100d3bc8bf2b4f54e95c443777cf8b6e1c4e9aad7f12a920f26a0fb83f3a136');
+
+export async function createSwap(customer, amount, preImageHash) {
+  const reward = 1;
+  const blocksBeforeCancelEnabled = 200;
+  const [from] = await getAccounts();
+  const txId = await contract.methods.createSwap(customer, amount, reward, preImageHash, blocksBeforeCancelEnabled).send({ from, gasPrice });
+  return txId;
+}
--- src/evm/web3.js
+++ src/evm/web3.js
@@ -1,9 +1,15 @@
 import Web3 from 'web3';
 import HDWalletProvider from 'truffle-hdwallet-provider';
+import SwapOffering from '@lncm/supercavitation-contracts/build/contracts/SwapOffering.json'
 
 import { evmUri, derevationPath } from '../../config.json';
 
 const mnemonic = process.env.MNEMONIC.trim();
 const provider = new HDWalletProvider(mnemonic, evmUri, 0, 1, false, derevationPath);
 
-export default new Web3(provider);
+const web3 = new Web3(provider);
+
+const contractAddress = SwapOffering.networks['31'].address;
+export const contract = new web3.eth.Contract(SwapOffering.abi, contractAddress);
+
+export default web3;
