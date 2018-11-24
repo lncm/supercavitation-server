b66bf978097f3e2657c8c1957fbc89283e145176
Chris Hitchcott <hitchcott@gmail.com>
2018-11-24 15:02:38 +0700

    add web3 example methods

--- src/evm/index.js
+++ src/evm/index.js
@@ -7,8 +7,14 @@ const mnemonic = process.env.MNEMONIC.trim();
 const provider = new HDWalletProvider(mnemonic, evmUri, 0, 1, false, derevationPath);
 const web3 = new Web3(provider);
 
-// eth.accounts().then(t => console.log(t)).catch(e => console.log(e));
+export async function getAccounts() {
+  const accounts = await web3.eth.getAccounts();
+  console.log('got accounts', accounts);
+  return accounts;
+}
 
-async function test() {
-  console.log('getting....', await web3.eth.getAccounts());
+export async function getBlockNumber() {
+  const block = await web3.eth.getBlockNumber();
+  console.log('got block', block);
+  return block;
 }
